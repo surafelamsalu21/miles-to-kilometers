f8e436cbb4898a62f323ff26388088a45c655498
Surafel <surafelamsalu2013@gmail.com>
2022-03-26 10:33:54 +0300

    The State of the app is changed from statless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-//You have no idea how i been working for this to work in the git repo
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+//The Bottom codes explains how stateful widgets are working
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Measures Converter',
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          title: Center(child: Text('Measures Converter')),
+        ),
+        body: Center(
+          child: Text('Measures Converter'),
         ),
       ),
     );
